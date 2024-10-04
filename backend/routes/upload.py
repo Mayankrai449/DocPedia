d8a3c8eefdee19f250cf1a055c6c6ce9af94e739
Mayank Rai <mayankraivns@gmail.com>
2024-10-04 11:40:14 +0530

    PerformanceOptimization for Indexing

--- backend/routes/upload.py
+++ backend/routes/upload.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, File, UploadFile, HTTPException, Depends
+from fastapi import APIRouter, File, UploadFile, HTTPException, Depends, BackgroundTasks
 from sqlalchemy.orm import Session
 from models.database import get_db
 from models.crud import create_document
@@ -9,21 +9,20 @@ import fitz
 router = APIRouter()
 
 @router.post("/upload")
-async def upload_file(file: UploadFile = File(...), db: Session = Depends(get_db)):
+async def upload_file(file: UploadFile = File(...), db: Session = Depends(get_db), background_tasks: BackgroundTasks = None):
     try:
         file_content = await file.read()
-        
-        if not upload_to_s3(file_content, file.filename):                               # Upload the file to S3
-            raise HTTPException(status_code=500, detail="Failed to upload file to S3")
-        
         document = create_document(db, file.filename)
 
         pdf_document = fitz.open(stream=file_content, filetype="pdf")
         text = "".join([page.get_text() for page in pdf_document])              # Extract text from PDF for intial indexing
-
         doc_id = index_document(file.filename, text)
+        
         if doc_id is None:
             raise HTTPException(status_code=500, detail="Failed to index document")
+        
+        background_tasks.add_task(upload_to_s3, file_content, file.filename)    # Upload the file to S3 in the background
+        
         return {"filename": file.filename, "document_id": document.id}
     except Exception as e:
         print(f"Detailed error: {str(e)}")
--- backend/services/query_indexing.py
+++ backend/services/query_indexing.py
@@ -1,6 +1,8 @@
 from llama_index.core import GPTVectorStoreIndex, Document
 from llama_index.embeddings.openai import OpenAIEmbedding
 from services.s3_reader import S3Reader
+import threading
+import time
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -10,20 +12,29 @@ openai_api_key = os.environ.get("OPENAI_API_KEY")
 embedding = OpenAIEmbedding(api_key=openai_api_key)
 
 index = None
+clear_timer = None
+timer = 600
+
+def clear_index():
+    global index
+    print("Clearing the index...")
+    index = None
 
 def index_document(filename, text):
-    global index
+    global index, clear_timer, timer
+    doc = Document(text=text, metadata={"filename": filename}) 
+    
     if index is None:
-        doc = Document(text=text, metadata={"filename": filename})                 # Create a compatible document format for Indexing
         index = GPTVectorStoreIndex.from_documents([doc], embedding=embedding)      # Use Vector Indexing to index the document
     else:
-        s3_reader = S3Reader()
-        documents = s3_reader.load_data()
-        
-        # Update the existing index with new documents
-        for doc in documents:
-            index.insert(doc)
-            
+        index.insert(doc)        # Add the document to the index        
+    
+    if clear_timer is not None:
+        clear_timer.cancel()
+
+    clear_timer = threading.Timer(timer, clear_index)
+    clear_timer.start()
+    
     return index
 
 def query_index(query):
